ffb8e8bb04b5cce85cb7cbdc67723a62e4e0ae09
Devansh Malik <devanshmalik@hotmail.com>
2022-02-24 21:25:31 -0500

    Create intermediate data from json files

--- src/d01_data/get_raw_data.py
+++ src/d01_data/get_raw_data.py
@@ -3,18 +3,15 @@ import os
 import requests
 import json
 import logging
+import pandas as pd
 
 log = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
-
 EIA_URL = "https://api.eia.gov/series/?series_id={}&api_key={}&out=json"
-# DATA_FOLDER = os.path.join(os.getcwd(), '../../data/01_raw/')
-# YML_FILE_PATH = os.path.join(os.getcwd(), '../../conf/base/', 'states.yml')
-
-# Only for jupyter notebook testing
 YML_FILE_PATH = os.path.join(os.getcwd(), '../conf/base/', 'states.yml')
-DATA_FOLDER = os.path.join(os.getcwd(), '../data/01_raw/')
+RAW_DATA_FOLDER = os.path.join(os.getcwd(), '../data/01_raw/')
+INTERMEDIATE_DATA_FOLDER = os.path.join(os.getcwd(), '../data/02_intermediate/')
 
 
 class EIADataPull:
@@ -39,22 +36,42 @@ class EIADataPull:
         return self.responses
 
     def save_data(self):
-        file_path = os.path.join(DATA_FOLDER, self.file_path)
+        file_path = os.path.join(RAW_DATA_FOLDER, self.file_path)
 
         for idx, state in enumerate(self.states):
-            with open(file_path.format(state), 'w', encoding='utf-8') as f:
+            with open(file_path.format(state, 'json'), 'w', encoding='utf-8') as f:
                 json.dump(self.responses[idx].json(), f, ensure_ascii=False, indent=4)
         return
 
-    def pull_data(self):
-        self.request_data()
-        self.save_data()
+    def create_intermediate_data(self):
+        raw_file_path = os.path.join(RAW_DATA_FOLDER, self.file_path)
+        intermediate_file_path = os.path.join(INTERMEDIATE_DATA_FOLDER, self.file_path)
+
+        for state in self.states:
+            with open(raw_file_path.format(state, 'json'), 'r') as f:
+                json_data = json.load(f)
+                df = pd.DataFrame(json_data['series'][0]['data'])
+                df.columns = ["date", "net_generation"]
+
+                # Convert year_quarter (2021Q3) into date ('2021-09-30') format
+                qs = df['date'].str.replace(r'(\d+)(Q\d)', r'\1-\2')
+                df['date'] = pd.PeriodIndex(qs, freq='Q').to_timestamp()
+                df['date'] = df['date'] + pd.offsets.QuarterEnd(0)
+
+                df.to_csv(intermediate_file_path.format(state, 'csv'), index=False)
 
 
-def get_raw_data():
+def load_data():
+    net_gen_id = 'ELEC.GEN.ALL-{}-99.Q'
+    file_path = 'Net_Gen_By_State/net_generation_{}.{}'
+
     log.info("Starting Raw Data Pull using EIA API...")
-    net_gen_ID = 'ELEC.GEN.ALL-{}-99.Q'
-    file_save_path = 'Net_Gen_By_State/net_generation_{}.json'
-    eia_data_pull = EIADataPull(series_id=net_gen_ID, file_path=file_save_path)
-    eia_data_pull.pull_data()
-    log.info("Finished Raw Data Pull.")
+    eia_data_pull = EIADataPull(series_id=net_gen_id, file_path=file_path)
+    eia_data_pull.request_data()
+    eia_data_pull.save_data()
+    log.info("Finished Raw Data Pull as JSON files.")
+
+    log.info("Creating Intermediate Data...")
+    eia_data_pull.create_intermediate_data()
+    log.info("Finished Creating Intermediate Data CSVs.")
+
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,5 @@
 from src.d00_utils.utils import setup_env_vars
-from src.d01_data.get_raw_data import get_raw_data
+from src.d01_data.get_raw_data import load_data
 
 setup_env_vars()
-get_raw_data()
+load_data()
