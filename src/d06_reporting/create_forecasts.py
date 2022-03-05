5ae5c8dc124da11bb017e3038bf2a7ad2ea4e35d
Devansh Malik <devanshmalik@hotmail.com>
2022-03-05 16:24:28 -0500

    Create individual and combined forecasts for all models

--- src/d06_reporting/create_forecasts.py
+++ src/d06_reporting/create_forecasts.py
@@ -15,37 +15,96 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 class ModelForecast:
     """Class to create forecasts based on models trained earlier"""
-    def __init__(self, save_folder, file_name):
-        self.save_folder = save_folder
-        self.file_name = file_name
-        self.states = STATES
+    net_gen_fuels = ['all_fuels', 'coal', 'natural_gas', 'nuclear',
+                     'hydro', 'wind', 'solar_all', 'other']
+    total_consumption_fuels = ['coal', 'natural_gas']
+    """Class to train Facebook Prophet models"""
+    def __init__(self, data_type):
+        self.data_type = data_type
+        self.save_folder = ""
 
     def forecast(self):
-        models_file_path = get_filepath(MODELS_FOLDER, self.save_folder, self.file_name)
-        reporting_file_path = get_filepath(REPORTING_FOLDER, self.save_folder, self.file_name)
+        for state in STATES:
+            log.info(f"Forecasting for State: {state}")
+            self.save_folder = '{}/{}'.format(self.data_type, state)
 
-        for state in self.states:
-            # Load Model
-            with open(models_file_path.format(state, 'json'), 'r') as fin:
-                model = model_from_json(json.load(fin))
+            if self.data_type == 'Net_Gen_By_Fuel_MWh':
+                fuel_types = ModelForecast.net_gen_fuels
+            elif self.data_type == "Fuel_Consumption_BTU":
+                fuel_types = ModelForecast.total_consumption_fuels
+            else:
+                raise ValueError(f"Unexpected EIA Data Type encountered: {self.data_type}")
 
-                # Makes a dataframe with both historical dates and
-                # append future dates for number of periods specified
-                future = model.make_future_dataframe(periods=16, freq='Q')
-                forecast = model.predict(future)
+            self.generate_individual_forecasts(fuel_types, state)
+            self.combine_forecasts(fuel_types, state)
+            log.info(f"Completed forecasting for State: {state}")
 
-                # Saving to CSV loses the datetime format causing errors when plotting
-                forecast.to_csv(reporting_file_path.format(state, 'csv'), index=False)
+    def generate_individual_forecasts(self, fuel_types, state):
+        for fuel_type in fuel_types:
+            model = self.read_prophet_model(fuel_type)
+            forecast = self.predict(model)
+            self.save_individual_forecast(forecast, fuel_type)
+        log.info(f"Finished individual forecasting for {self.data_type}")
+
+    def read_prophet_model(self, fuel_type):
+        models_file_name = '{}-{}.{}'.format(self.data_type, fuel_type, 'json')
+        models_file_path = get_filepath(MODELS_FOLDER, self.save_folder, models_file_name)
+        with open(models_file_path, 'r') as fin:
+            return model_from_json(json.load(fin))
+
+    @staticmethod
+    def predict(model):
+        future = model.make_future_dataframe(periods=16, freq='Q')
+        forecast = model.predict(future)
+
+        # Create column y with historical values for periods in past with future predictions for future periods
+        forecast['y'] = model.history['y'].combine_first(forecast['yhat'])
+        return forecast
+
+    def save_individual_forecast(self, forecast, fuel_type):
+        # Create new parent folder for all individual forecasts
+        reporting_folder = 'Individual_Forecasts/{}'.format(self.save_folder)
+        reporting_file_name = '{}-{}.{}'.format(self.data_type, fuel_type, 'csv')
+        reporting_file_path = get_filepath(REPORTING_FOLDER, reporting_folder, reporting_file_name)
+
+        # Saving to CSV loses the datetime format causing errors when plotting
+        forecast.to_csv(reporting_file_path, index=False)
+
+    def combine_forecasts(self, fuel_types, state):
+        log.info(f"Combining individual forecasts for {self.data_type}")
+        df_combined = pd.DataFrame()
+        # TODO: Do not add all_fuels to dataframe
+        for fuel in fuel_types:
+            forecast = self.read_forecast(fuel)
+            if 'date' not in df_combined.columns:
+                df_combined['date'] = forecast['ds']
+            df_combined[fuel] = forecast['y']
+        self.save_combined_forecast(df_combined, state)
+
+    def read_forecast(self, fuel_type):
+        # Create new parent folder for all individual forecasts
+        reporting_folder = 'Individual_Forecasts/{}'.format(self.save_folder)
+        reporting_file_name = '{}-{}.{}'.format(self.data_type, fuel_type, 'csv')
+        reporting_file_path = get_filepath(REPORTING_FOLDER, reporting_folder, reporting_file_name)
+
+        return pd.read_csv(reporting_file_path)
+
+    def save_combined_forecast(self, df_combined, state):
+        # Create new parent folder for all combined forecasts
+        reporting_folder = 'Combined_Forecasts/{}'.format(state)
+        reporting_file_name = '{}-Combined.{}'.format(self.data_type, 'csv')
+        reporting_file_path = get_filepath(REPORTING_FOLDER, reporting_folder, reporting_file_name)
+
+        # Saving to CSV loses the datetime format causing errors when plotting
+        df_combined.to_csv(reporting_file_path, index=False)
 
 
-def create_forecasts():
-    save_folder = 'Net_Gen_By_State'
-    file_name = 'net_generation_{}.{}'
-
-    log.info("Creating forecasts using trained Prophet models...")
-    data_process = ModelForecast(save_folder=save_folder, file_name=file_name)
-    data_process.forecast()
-    log.info("Finished creating and saving forecasts.")
+def create_all_forecasts(eia_api_ids):
+    log.info("Creating forecasts for all Prophet Models...")
+    for data_type in eia_api_ids.keys():
+        model_forecaster = ModelForecast(data_type=data_type)
+        model_forecaster.forecast()
+    log.info("Finished all forecasting")
 
 
 # Non-interactive plots
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,4 +1,6 @@
 #TODO: Create an interface class to run different steps of model pipeline
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from src.d00_utils.const import *
 from src.d00_utils.utils import setup_env_vars, load_yml
@@ -6,15 +8,14 @@ from src.d01_data.get_raw_data import load_all_data
 from src.d02_intermediate.clean_raw_data import clean_all_data
 from src.d03_processing.create_model_input import process_all_data
 from src.d04_modelling.create_prophet_models import train_all_models
-from src.d06_reporting.create_forecasts import create_forecasts
+from src.d06_reporting.create_forecasts import create_all_forecasts
 
 setup_env_vars()
 eia_api_ids = load_yml(EIA_API_IDS_YML_FILEPATH)
 # load_all_data(eia_api_ids)
 # clean_all_data(eia_api_ids)
 # process_all_data(eia_api_ids)
-train_all_models(eia_api_ids)
+# train_all_models(eia_api_ids)
+create_all_forecasts(eia_api_ids)
 
-# Need to be updated to new upstream data structure
-# create_forecasts()
 
